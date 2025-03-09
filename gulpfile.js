7da89726a5bbe63ce338a3f991e88d3f1f83afbd
isclarke <156957402+isclarke@users.noreply.github.com>
2025-03-09 18:44:32 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -8,16 +8,45 @@ const htmlclean = require(`gulp-htmlclean`);
 const connect = require(`gulp-connect`);
 const sourcemaps = require(`gulp-sourcemaps`);
 const fs = require(`fs`);
+const gulpIf = require(`gulp-if`);
 
 // Ensure 'prod/js' and 'prod/css' directories exist
 let createDirs = (done) => {
-    const dirs = [`prod/js`, `prod/css`];
+    const dirs = [`prod/js`, `prod/css`, `prod/img`, `prod/assets`];
     dirs.forEach(dir => {
         if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir, { recursive: true });
         }
     });
     done();
+};
+
+// Linting CSS files
+let lintCSS = () => {
+    return gulp.src(`src/css/**/*.css`)
+        .pipe(stylelint({
+            failAfterError: false,
+        }));
+};
+
+// Scripts task (JS processing for production and development)
+let scripts = () => {
+    return gulp.src(`src/js/**/*.js`)
+
+        .pipe(babel({ presets: [`@babel/preset-env`] }))
+        .pipe(gulp.dest(`prod/js`));
+};
+
+// Styles task (CSS processing for production and development)
+let styles = () => {
+    return gulp.src(`src/css/**/*.css`)
+        .pipe(gulp.dest(`prod/css`));
+};
+
+// HTML task (minify HTML for production)
+let html = () => {
+    return gulp.src(`index.html`)
+        .pipe(gulp.dest(`prod`));
 };
 
 let lintJS = () => {
@@ -27,37 +56,7 @@ let lintJS = () => {
         .pipe(eslint.failAfterError());
 };
 
-let lintCSS = () => {
-    return gulp.src(`src/css/**/*.css`)
-        .pipe(stylelint({
-            failAfterError: false,
-        }));
-};
-
-
-let scripts = () => {
-    return gulp.src(`src/js/**/*.js`)
-        .pipe(sourcemaps.init())
-        .pipe(babel({ presets: [`@babel/preset-env`] }))
-        .pipe(uglify())
-        .pipe(sourcemaps.write(`.`))
-        .pipe(gulp.dest(`prod/js`));
-};
-
-let styles = () => {
-    return gulp.src(`src/css/**/*.css`)
-        .pipe(sourcemaps.init())
-        .pipe(cleanCSS())
-        .pipe(sourcemaps.write(`.`))
-        .pipe(gulp.dest(`prod/css`));
-};
-
-let html = () => {
-    return gulp.src(`index.html`)
-        .pipe(htmlclean())
-        .pipe(gulp.dest(`prod`));
-};
-
+// Watch files during development
 let watchFiles = () => {
     connect.server({ livereload: true });
     gulp.watch(`src/js/**/*.js`, gulp.series(lintJS, scripts));
@@ -65,6 +64,15 @@ let watchFiles = () => {
     gulp.watch(`index.html`, gulp.series(html));
 };
 
+// Build production folder (no linting, compressing everything)
+let buildProd = gulp.series(
+    createDirs,
+    html,            // Minify HTML
+    styles,          // Minify CSS
+    scripts,         // Minify JS
+);
+
+// Export tasks
 exports.lint = gulp.parallel(lintJS, lintCSS);
-exports.build = gulp.series(createDirs, html, styles, scripts);
-exports.default = gulp.series(exports.lint, watchFiles);
+exports.build = gulp.series(buildProd); // Run production build
+exports.default = gulp.series(exports.lint, watchFiles); // Run linting and watch files in development
