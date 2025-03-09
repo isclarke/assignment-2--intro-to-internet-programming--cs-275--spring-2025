ad6b3fe2ff614295c728af075b65e775e0683680
isclarke <156957402+isclarke@users.noreply.github.com>
2025-03-09 18:15:42 -0400

    fix syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
-const gulp = require('gulp');
-const eslint = require('gulp-eslint');
-const stylelint = require('gulp-stylelint');
-const cleanCSS = require('gulp-clean-css');
-const uglify = require('gulp-uglify');
-const babel = require('gulp-babel');
-const htmlclean = require('gulp-htmlclean');
-const connect = require('gulp-connect');
-const sourcemaps = require('gulp-sourcemaps');
-const fs = require('fs');
-const path = require('path');
+const gulp = require(`gulp`);
+const eslint = require(`gulp-eslint`);
+const stylelint = require(`gulp-stylelint`);
+const cleanCSS = require(`gulp-clean-css`);
+const uglify = require(`gulp-uglify`);
+const babel = require(`gulp-babel`);
+const htmlclean = require(`gulp-htmlclean`);
+const connect = require(`gulp-connect`);
+const sourcemaps = require(`gulp-sourcemaps`);
+const fs = require(`fs`);
 
 // Ensure 'prod/js' and 'prod/css' directories exist
 let createDirs = (done) => {
-    const dirs = ['prod/js', 'prod/css'];
+    const dirs = [`prod/js`, `prod/css`];
     dirs.forEach(dir => {
         if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir, { recursive: true });
@@ -22,52 +21,54 @@ let createDirs = (done) => {
 };
 
 let lintJS = () => {
-    return gulp.src('src/js/**/*.js')
+    return gulp.src(`src/js/**/*.js`)
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
 };
 
 let lintCSS = () => {
-    return gulp.src('src/css/**/*.css')
+    return gulp.src(`src/css/**/*.css`)
         .pipe(stylelint({
+            failAfterError: false,
             reporters: [
                 {
-                    formatter: 'string',
+                    formatter: `string`,
                     console: true
                 }
             ]
         }));
 };
 
+
 let scripts = () => {
-    return gulp.src('src/js/**/*.js')
+    return gulp.src(`src/js/**/*.js`)
         .pipe(sourcemaps.init())
-        .pipe(babel({ presets: ['@babel/preset-env'] }))
+        .pipe(babel({ presets: [`@babel/preset-env`] }))
         .pipe(uglify())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('prod/js'));
+        .pipe(sourcemaps.write(`.`))
+        .pipe(gulp.dest(`prod/js`));
 };
 
 let styles = () => {
-    return gulp.src('src/css/**/*.css')
+    return gulp.src(`src/css/**/*.css`)
         .pipe(sourcemaps.init())
         .pipe(cleanCSS())
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('prod/css'));
+        .pipe(sourcemaps.write(`.`))
+        .pipe(gulp.dest(`prod/css`));
 };
 
 let html = () => {
-    return gulp.src('index.html')
+    return gulp.src(`index.html`)
         .pipe(htmlclean())
-        .pipe(gulp.dest('prod'));
+        .pipe(gulp.dest(`prod`));
 };
 
 let watchFiles = () => {
     connect.server({ livereload: true });
-    gulp.watch('src/js/**/*.js', gulp.series(lintJS, scripts));
-    gulp.watch('src/css/**/*.css', gulp.series(lintCSS, styles));
-    gulp.watch('index.html', gulp.series(html));
+    gulp.watch(`src/js/**/*.js`, gulp.series(lintJS, scripts));
+    gulp.watch(`src/css/**/*.css`, gulp.series(lintCSS, styles));
+    gulp.watch(`index.html`, gulp.series(html));
 };
 
 exports.lint = gulp.parallel(lintJS, lintCSS);
