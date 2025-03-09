d8e1e2b556f687fbfa39560c82bba0dc1c2e9112
isclarke <156957402+isclarke@users.noreply.github.com>
2025-03-09 15:32:42 -0400

    add styles, watchfiles, html, and scripts

--- gulpfile.js
+++ gulpfile.js
@@ -21,3 +21,34 @@ let lintCSS = () => {
             reporters: [{ formatter: 'string', console: true }]
         }));
 };
+
+let scripts = () => {
+    return gulp.src('main.js')
+        .pipe(sourcemaps.init())
+        .pipe(babel({ presets: ['@babel/preset-env'] }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('prod'));
+};
+
+let styles = () => {
+    return gulp.src('main.css')
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('prod'));
+};
+
+let html = () => {
+    return gulp.src('index.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('prod'));
+};
+
+let watchFiles = () => {
+    connect.server({ livereload: true });
+    gulp.watch('main.js', gulp.series(lintJS, scripts));
+    gulp.watch('main.css', gulp.series(lintCSS, styles));
+};
+
+exports.lint = gulp.parallel(lintJS, lintCSS);
+exports.build = gulp.series(html, styles, scripts);
+exports.default = gulp.series(exports.lint, watchFiles);
