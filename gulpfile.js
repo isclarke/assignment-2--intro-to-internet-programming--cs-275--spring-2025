1dcf2d431dc4c5cad5d73687a7bc90a2e9236c3f
isclarke <156957402+isclarke@users.noreply.github.com>
2025-03-09 15:31:31 -0400

    add lint for js and css

--- gulpfile.js
+++ gulpfile.js
@@ -7,3 +7,17 @@ const babel = require('gulp-babel');
 const htmlmin = require('gulp-htmlmin');
 const connect = require('gulp-connect');
 const sourcemaps = require('gulp-sourcemaps');
+
+let lintJS = () => {
+    return gulp.src('main.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+};
+
+let lintCSS = () => {
+    return gulp.src('main.css')
+        .pipe(stylelint({
+            reporters: [{ formatter: 'string', console: true }]
+        }));
+};
